c7ecc02bed1ab42895da16aacf1a91b16ab5ae4d
Guillermo-31 <69165809+Guillermo-31@users.noreply.github.com>
2022-07-08 11:22:12 -0600

    Ejercicio1

---
+++ guias/Guía 1/ejercicios/js/ejercicio1.js
@@ -0,0 +1,5 @@
+document.write("<main>")
+document.write("<header><h1>Header</h1></header>");
+document.write("<div id='container'><nav><h1>Navigation</h1></nav><div id='container-articles'><article><h1>ARTICLE</h1></article><article><h1>ARTICLE</h1></article><article><h1>ARTICLE</h1></article></div></div>");
+document.write("<footer><h1>footer</h1></footer>");
+document.write("</main>")
