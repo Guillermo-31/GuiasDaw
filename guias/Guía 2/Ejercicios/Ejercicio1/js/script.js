9d40f42dea7c40f5f52284fb4cb19af6a218864b
cr1ss7 <69165809+Guillermo-31@users.noreply.github.com>
2022-07-14 15:59:14 -0600

    Actu

---
+++ guias/Guía 2/Ejercicios/Ejercicio1/js/script.js
@@ -0,0 +1,9 @@
+let num, cantcifras, cantim, cantpar, sumim, sumpar, sumtotal, ciframayor, ciframenor, cifras = new Array;
+
+num = prompt('Ingrese el número entero que quiera: ','');
+
+for (let i = 0; i <= 9; i++) {
+  if (cantcifras == num){
+    
+  }
+}
