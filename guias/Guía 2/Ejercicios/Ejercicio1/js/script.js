e84e390007c77afc51aa1d5f0c28249a759a6b4d
Guillermo-31 <69165809+Guillermo-31@users.noreply.github.com>
2022-07-15 08:22:02 -0600

    actu

--- guias/Guía 2/Ejercicios/Ejercicio1/js/script.js
+++ guias/Guía 2/Ejercicios/Ejercicio1/js/script.js
@@ -1,9 +1,8 @@
-let num, cantcifras, cantim, cantpar, sumim, sumpar, sumtotal, ciframayor, ciframenor, cifras = new Array;
-
+let num, axuiliar, mayorC, menorC, sumC, cifrasIm, cifrasPar, impares, pares, i, j, numCifras, cantCifras, continuar  = true, cifras;
 num = prompt('Ingrese el número entero que quiera: ','');
 
-for (let i = 0; i <= 9; i++) {
-  if (cantcifras == num){
-    
-  }
+cifras = num.split('');
+
+for (i = 0;  i < cifras.length; i++) {
+  alert("La cfira "+i+" es: "+cifras[i]);
 }
