8779702b1da4c374eb8b8fbf5e53fd4295e55855
cr1ss7 <69165809+Guillermo-31@users.noreply.github.com>
2022-07-15 11:19:57 -0600

    actu

--- guias/Guía 2/Ejercicios/Ejercicio1/js/script.js
+++ guias/Guía 2/Ejercicios/Ejercicio1/js/script.js
@@ -1,8 +1,7 @@
-let num, axuiliar, mayorC, menorC, sumC, cifrasIm, cifrasPar, impares, pares, i, j, numCifras, cantCifras, continuar  = true, cifras;
-num = prompt('Ingrese el número entero que quiera: ','');
+let num, axuiliar, mayorC, menorC, sumC, cifrasIm, cifrasPar, impares, pares, i, j, numCifras, cantCifras, negativo = false, cifras;
+num = Math.abs(prompt('Ingrese el número entero que quiera: ')).toFixed(0);
+cantCifras = num.length;
 
-cifras = num.split('');
-
-for (i = 0;  i < cifras.length; i++) {
-  alert("La cfira "+i+" es: "+cifras[i]);
+for (i = 0;  i < num.length; i++) {
+  alert("La cifra "+i+" es: "+ num[i]);
 }
